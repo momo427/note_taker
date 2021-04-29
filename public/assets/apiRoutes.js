5922af05264ac5a938ef601c9cd2b8bd05a0d0b1
momogit <ferguson.tyra@gmail.com>
2021-04-29 11:08:58 -0400

    working on routes to html and server

---
+++ public/assets/apiRoutes.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = app => {
+    fs.readFile("db/db.json","utf8", (err, data) =>{
+        if (err) throw err;
+
+        var notes = JSON.parse(data);
+
+
+        //api/notes get route
+        app.get("/api/notes", function(req, res){
+
+            res.json(notes);
+        });
+
+        ///api/notes post route
+        app.post("/api/notes", function(req, res){
+
+            let newNote = req.body;
+            notes.push(newNote);
+            update();
+            return console.log("Added new note:"+newNote.title)
+        });
+
+    })
+}
---
+++ public/assets/htmlRoutes.js
@@ -0,0 +1,19 @@
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+
+const path = require('path');
+
+// ROUTING
+
+module.exports = (app) => {
+  // => HTML GET Requests
+
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
+
+  // If no matching route is found default to home
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+};
